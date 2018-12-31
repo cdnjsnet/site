be97b7f0efd3c832aeb32a9723007f68a7a2892e
xiaoq-in <g@xiaoq.in>
2019-01-01 01:26:04 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -94,7 +94,7 @@ var appPackage1 = new Vue({
 (function () {
     //Ajax请求所有列表数据，并存于root.data下
     var request = new XMLHttpRequest();
-    request.open("GET", "https://api.bootcdn.cn/libraries.min.json");
+    request.open("GET", "../api/libraries.min.json");
     request.send();
     request.onreadystatechange = function () {
         if (request.readyState === 4 && request.status === 200) {
